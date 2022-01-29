3133eeeafeef99c37e8f4863e27eafe676949609
AAB87 <85685002+AAB-87@users.noreply.github.com>
2022-01-29 19:01:19 +0000

    fixed array

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-let salary = [5 * 10000, 15 * 25000, 40 * 50000, 10 * 100000];
-let sum = 0;
-for(let i = 0; i < salary.length; i++) {
-    sum += salary[i]
+
+let salaries = [];
+
+for (let i = 0; i < 5; i++) {
+	salaries.push(10000)
 }
+
+for (let i = 0; i < 15; i++) {
+	salaries.push(25000)
+}
+
+for (let i = 0; i < 40; i++) {
+	salaries.push(50000)
+}
+
+for (let i = 0; i < 10; i++) {
+	salaries.push(100000)
+}
+
+let sum = 0
+for(let i = 0; i < salaries.length; i++) {
+    sum += salaries[i]
+}
+
 let taxPercentage = 20;
 percentageOfAllSalaries = sum / 100 * taxPercentage;
 console.log(percentageOfAllSalaries);
+
 let staff = 70;
 let averagePercentage = Math.floor(percentageOfAllSalaries / staff);
 console.log(averagePercentage);
